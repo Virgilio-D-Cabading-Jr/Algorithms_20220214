b2eeaf6834c9f07b441f0d3fe28c334af56c7f77
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-14 16:38:51 -0800

    add test 1

--- algo.js
+++ algo.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
-    
+    let longestLength = 0;
+
+    return longestLength;
 };
+
+const string1 = "abcabcbb";
+
+console.log(`lengthOfLongestSubstring ${string1}`,lengthOfLongestSubstring(string1))
