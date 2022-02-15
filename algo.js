d9ca254619323042f839d4f01b25f0c2acb1c9bf
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-14 16:57:31 -0800

    remove debuggin console.log

--- algo.js
+++ algo.js
@@ -8,12 +8,14 @@ const lengthOfLongestSubstring = (s) => {
     for (let x=0; x<s.length; x++) {
         let substringWithNoRepeat = "";
         for (let y=x; y<s.length; y++) {
-            if (!s.includes(s[y])) {
+            if (!substringWithNoRepeat.includes(s[y])) {
                 substringWithNoRepeat += s[y];
+                if (substringWithNoRepeat.length > longestLength) {
+                    longestLength = substringWithNoRepeat.length;
+                }
             } else {
                 substringWithNoRepeat = "";
             }
-            console.log(substringWithNoRepeat);
         }
     }
 
